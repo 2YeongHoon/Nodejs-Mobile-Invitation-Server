496e5c61345ff6449a3bf63763f6efbe15bc4a20
2YoungHoon <djaakclsrn12@naver.com>
2021-12-22 08:23:58 +0900

    User Read Add

--- src/entity/Images.ts
+++ src/entity/Images.ts
@@ -1,23 +1,30 @@
-import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, BaseEntity, JoinColumn} from "typeorm";
-import { WeddingInfo } from "./WeddingInfo";
-export type ImageTypeEnum = "image"|"main"|"map"
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  BaseEntity,
+  JoinColumn
+} from 'typeorm'
+import { WeddingInfo } from './WeddingInfo'
+export type ImageTypeEnum = 'image' | 'main' | 'map'
 
 @Entity()
 export class Images extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    idx: number;
-    
-    @ManyToOne(() => WeddingInfo, (weddingInfo) => weddingInfo.idx)
-    @JoinColumn()
-    weddingInfo: WeddingInfo;
+  @PrimaryGeneratedColumn()
+  idx: number
 
-    @Column({nullable: true})
-    imageKey: string;
+  @ManyToOne(() => WeddingInfo, weddingInfo => weddingInfo.idx)
+  @JoinColumn()
+  weddingInfo: WeddingInfo
 
-    @Column({
-        type: "set",
-        enum: ["image","main","map"],
-        default: ["image"]
-    })
-    imageType: ImageTypeEnum[]
+  @Column({ nullable: true })
+  imageKey: string
+
+  @Column({
+    type: 'set',
+    enum: ['image', 'main', 'map'],
+    default: ['image']
+  })
+  imageType: ImageTypeEnum[]
 }
--- src/routes/UserCreate.ts
+++ src/routes/UserCreate.ts
@@ -1,27 +1,39 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import { userInfo } from "os";
-import { User } from "../entity/User";
-import { WeddingInfo } from "../entity/WeddingInfo";
-import { Images } from "../entity/Images";
-import { Videos } from "../entity/Videos";
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import { userInfo } from 'os'
+import { User } from '../entity/User'
+import { WeddingInfo } from '../entity/WeddingInfo'
+import { Images } from '../entity/Images'
+import { Videos } from '../entity/Videos'
 
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-// 최종 저장 Create
+// Create
 router.post('/user', async (req, res) => {
-	console.log("hello", req.body);
-	try {
-		await User.save(req.body);
-		await WeddingInfo.save(req.body);
-		res.send("success");
-	} catch(e){
-		res.send({
-			message: e
-		});
-		throw new Error(e);
-	}
+  try {
+    await User.save(req.body)
+    await WeddingInfo.save(req.body)
+    res.send('success')
+  } catch (e) {
+    res.send({
+      message: e
+    })
+    throw new Error(e)
+  }
 })
 
-module.exports = router;
+// Read
+router.get('/user', async (req, res) => {
+  try {
+    const users = await User.find(req.body)
+    res.send(users)
+  } catch (e) {
+    res.send({
+      message: e
+    })
+    throw new Error(e)
+  }
+})
+
+module.exports = router
