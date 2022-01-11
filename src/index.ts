b577d42b41e7737d24bf1a253bcd62a676d81940
Kuby <thtjwls@gmail.com>
2022-01-11 23:15:08 +0900

    es 모듈형태로 통일

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import 'reflect-metadata'
 import { TypeORMError } from 'typeorm'
 import { createConnection } from 'typeorm'
 import * as bodyParser from 'body-parser'
+import * as express from 'express'
+import router_1 from './router/User'
+import router_2 from './router/Save'
 
 // Connect typeORM Database
 console.log('Database Connection....')
@@ -11,16 +14,13 @@ createConnection()
   })
   .catch(error => console.log('Database Connection....Fail', error))
 
-var express = require('express')
 // Create express server
 const app = express()
-var router_1 = require('./router/User')
-var router_2 = require('./router/Save')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router_1)
-app.user(router_2)
+app.use(router_2)
 
 // //setting port
 // app.set("port", process.env.PORT || 3000);
@@ -28,4 +28,5 @@ app.user(router_2)
 app.listen(3000, () => {
   console.log('listen to 3000')
 })
+
 export default app
